185f1eaa2a05b4a676512b2accf65885d9cdd448
Karl <definitelynew2chess@gmail.com>
2025-11-03 21:22:23 -0500

    Added Russian and Chinese localization
    Thanks to DingXiang233 for the Chinese translation and xsSplater_СанСаныч for the Russian translation!

--- CircumstanceFix/scripts/mods/CircumstanceFix/CircumstanceFix_localization.lua
+++ CircumstanceFix/scripts/mods/CircumstanceFix/CircumstanceFix_localization.lua
@@ -1,9 +1,12 @@
 return {
     mod_name = {
         en = "Circumstance Fix",
+        ru = "Исправление: Текст обстоятельств миссий",
+        ["zh-cn"] = "环境词条修复"
     },
     mod_description = {
         en = "Fixes visual bugs with overlapping text in the mission terminal",
-    },
+        ru = "Circumstance Fix - Исправляет визуальные ошибки с накладывающимся текстом в терминале миссии.",
+        ["zh-cn"] = "修复任务终端中文本重叠显示的视觉错误"
+    }
 }
-
